a1d58404ee4ba5518552adee7abfa43229f7cf81
ap0phasi <66261738+ap0phasi@users.noreply.github.com>
2024-06-01 21:29:27 +1000

    Bug Fix: Norm Group Arrangement
    Norm Group arrangement is corrected, also initial transition tensor is now nonzero.
    
    This commit also includes a dump of dev notebooks.

--- coilspy/neuralcoil/neuralcoil.py
+++ coilspy/neuralcoil/neuralcoil.py
@@ -11,7 +11,7 @@ class NeuralCoilLayer(nn.Module):
         self.interaction_tensors = nn.Parameter(torch.randn(n_features, n_features, n_features, n_features + 1))
         self.topk_num = 1
         
-        starting_tensor = torch.zeros(n_batch, n_features, n_features)
+        starting_tensor = torch.softmax(torch.zeros(n_batch, n_features, n_features), dim = 1)
         if device == "cuda":
             self.starting_transition_tensor = starting_tensor.to("cuda")
         else:
@@ -19,25 +19,22 @@ class NeuralCoilLayer(nn.Module):
         
     def step_coil(self, state_tensor, previous_transition_tensor):
         # Establish normalized subgroups
-        norm_subgroups = torch.cat((state_tensor.unsqueeze(-2), previous_transition_tensor), dim=1)
+        norm_subgroups = torch.cat((state_tensor.unsqueeze(-1), previous_transition_tensor), dim=2)
         batch_size, num_groups, n_features = norm_subgroups.shape
         
         # Compute scores for each normalized subgroup
-        scores = self.act(self.attention_weights(norm_subgroups)).sum(-1) # [batch_size, num_groups]
+        scores = self.act(self.attention_weights(norm_subgroups.permute(0,2,1))).sum(-1) # [batch_size, num_groups]
         
-        weights = torch.softmax(scores,dim = -1) # [batch_size, num_groups]
+        weights = torch.softmax(scores, dim = -1) # [batch_size, num_groups]
         
         selected_interaction_tensors = self.interaction_tensors
         selected_norm_subgroups = norm_subgroups
 
-        #selected_transition_tensors = torch.einsum('ijks,bsi->bjks', selected_interaction_tensors, selected_norm_subgroups)
-        
-        selected_transition_tensors = (torch.mul(selected_interaction_tensors, selected_norm_subgroups.permute(0,2,1).unsqueeze(1).unsqueeze(1))).sum(-2)
+        selected_transition_tensors = (torch.mul(selected_interaction_tensors, selected_norm_subgroups.unsqueeze(1).unsqueeze(1))).sum(-2)
         
         # We need a single transition tensor so we will average this as well
-        #selected_transition_tensor = torch.einsum('bs,bjks->bjk', weights,selected_transition_tensors)
-        
         selected_transition_tensor = (torch.mul(selected_transition_tensors, weights.unsqueeze(-2).unsqueeze(-2))).sum(-1)
+
         # Generate state tensor from the transition tensor
         # Unsqueezing state_tensor to make it [batch_size, n_features, 1] for matrix multiplication
         state_tensor_unsqueezed = state_tensor.unsqueeze(2)
@@ -47,7 +44,7 @@ class NeuralCoilLayer(nn.Module):
 
         # Squeezing the result to get rid of the extra dimension, resulting in [batch_size, n_features]
         new_state_tensor = new_state_tensor_bmm.squeeze(2)
-    
+        
         softmax_tensor = torch.softmax(new_state_tensor * n_features, dim = 1)
         
         return softmax_tensor, selected_transition_tensor
